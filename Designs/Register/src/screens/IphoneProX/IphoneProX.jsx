bf8aaa0292813b9e13c9a805135bdbd6b681e314
Joe_dw <joedewet36@gmail.com>
2023-06-30 14:38:45 +0200

    styling changes

---
+++ Designs/Register/src/screens/IphoneProX/IphoneProX.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./style.css";
+
+export const IphoneProX = () => {
+  return (
+    <div className="iphone-pro-x">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="ellipse" />
+            <h1 className="text-wrapper">Register</h1>
+            <img className="chevron-left" alt="Chevron left" src="/img/chevron-left.svg" />
+            <img className="group" alt="Group" src="/img/group-772.png" />
+          </div>
+          <div className="text-wrapper-2">username</div>
+        </div>
+        <div className="div-wrapper">
+          <div className="text-wrapper-3">muncher</div>
+        </div>
+        <div className="overlap-2">
+          <div className="text-wrapper-4">your password</div>
+          <img className="vector" alt="Vector" src="/img/vector.svg" />
+        </div>
+        <div className="frame">
+          <div className="text-wrapper-5">Register</div>
+        </div>
+        <div className="frame-2">
+          <div className="text-wrapper-6">Have an account?</div>
+          <div className="text-wrapper-7">Login</div>
+        </div>
+        <div className="text-wrapper-8">password</div>
+        <div className="overlap-3">
+          <a className="text-wrapper-4" href="mailto:yourmail@mail.com" rel="noopener noreferrer" target="_blank">
+            yourmail@mail.com
+          </a>
+        </div>
+        <div className="text-wrapper-9">email</div>
+        <div className="dark-mode-false-type">
+          <div className="right-side">
+            <img className="battery" alt="Battery" src="/img/battery.png" />
+            <img className="wifi" alt="Wifi" src="/img/wifi.svg" />
+            <img className="mobile-signal" alt="Mobile signal" src="/img/mobile-signal.svg" />
+          </div>
+          <img className="left-side" alt="Left side" src="/img/left-side.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
